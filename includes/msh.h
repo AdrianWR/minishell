3ca842b84cc206fc38b67876047bbbac9a54f529
GustavoAriadno <gugu.gustavo2011@gmail.com>
2020-11-04 10:11:48 +0100

    Structure a basic project

---
+++ includes/msh.h
@@ -0,0 +1,11 @@
+#ifndef MSH_H
+# define MSH_H
+
+# include <stdio.h>
+# include <limits.h>
+# include <unistd.h>
+# include "get_next_line.h"
+# include "libft.h"
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "msh.h"
+
+int	main()
+{
+	// Load config files, if any.
+
+	// Run command loop.
+
+	// Perform any shutdown/cleanup.
+
+	
+	char cwd[PATH_MAX];
+	if (getcwd(cwd, sizeof(cwd)) != NULL)
+		printf("Current working dir: %s\n", cwd);
+	else
+		perror("getcwd() error");
+
+	return 0;
+}
