071b95b16243c6ef545697cdfcb1121e3e05a26c
Adrian W. Roque <adrian.w.roque@gmail.com>
2020-12-15 21:16:52 -0300

    Break norminette

--- src/main.c
+++ src/main.c
@@ -6,11 +6,17 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 17:33:31 by aroque            #+#    #+#             */
-/*   Updated: 2020/11/08 17:34:04 by aroque           ###   ########.fr       */
+/*   Updated: 2020/12/15 21:16:18 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+// HEHEHE Vou quebrar a norminette
+// Boa Davi
+// Boa Victor
+// Boa Salem
+// Wow!!! :D
 
 int	main(void)
 {
