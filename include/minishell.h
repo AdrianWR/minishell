5c85d64c79cb4cef9247cf7bc0796886917ea569
Adrian W. Roque <adrian.w.roque@gmail.com>
2021-01-25 09:20:38 -0300

    Create tokenizer header file
    Implement tokenizer

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gariadno <gariadno@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 23:16:10 by aroque            #+#    #+#             */
-/*   Updated: 2021/01/20 02:40:03 by gariadno         ###   ########.fr       */
+/*   Updated: 2021/01/25 09:19:42 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,9 @@
 # include "libft.h"
 # include "hash.h"
 
-typedef enum	e_type
-{
-	COMMAND,
-	PARAMETER,
-	OPERATOR
-}				t_type;
-
-typedef struct	s_token
-{
-	char		*command;
-}				t_token;
-
 void			create_process(char *const *argv, char **envp,
 					t_hashtable *env);
 void			sighandler(int signum);
-char			**tokenizer(char *input);
 t_hashtable		*load_env(char *envp[]);
 void			repl(t_hashtable *env, char **envp);
 char			**getpaths(const char *argv, const char *path);
