e655643fdaaad0562b5a5ee1433e6b83b37164c2
Adrian W. Roque <adrian.w.roque@gmail.com>
2021-02-01 00:38:25 -0300

    Add lexer unit tests
    Add unfinished quote error code

--- include/errcode.h
+++ include/errcode.h
@@ -6,7 +6,7 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 23:36:03 by aroque            #+#    #+#             */
-/*   Updated: 2021/01/05 21:29:22 by aroque           ###   ########.fr       */
+/*   Updated: 2021/01/31 00:51:59 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ typedef enum		e_errcode {
 	ERRSYS,
 	EUSAGE,
 	ECMDNF,
+	EUNFQT,
 	__ERRMAX
 }					t_errcode;
 
 static char *const	g_errstr[__ERRMAX] = {
 	"__ERRSYS",
 	"Usage: ./minishell",
-	"Command not found..."
+	"Command not found...",
+	"Unfinished quote"
 };
 
 void				message_and_exit(t_errcode code, char *note);
--- test/src/test.c
+++ test/src/test.c
@@ -6,15 +6,31 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 20:27:24 by aroque            #+#    #+#             */
-/*   Updated: 2021/01/25 09:15:44 by aroque           ###   ########.fr       */
+/*   Updated: 2021/02/01 00:18:45 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include "hash.h"
 #include "minishell.h"
 #include "minunit.h"
 #include "libft.h"
 #include "tokenizer.h"
+
+t_hashtable	*htenv;
+
+void			setup(void)
+{
+	htenv = ht_create(128);
+	ht_set(htenv, "USER", ft_strdup("gariadno"));
+	ht_set(htenv, "SHELL", ft_strdup("minishell"));
+	ht_set(htenv, "CAKE", ft_strdup("strawberry"));
+}
+
+void	teardown(void)
+{
+	ht_free(htenv, free);
+}
 
 MU_TEST(test_ft_strspn)
 {
@@ -49,74 +65,95 @@ MU_TEST(test_ft_strtok)
 	mu_assert_string_eq(ft_strtok(str, " "), "This");
 }
 
-//static void lst_compare(t_list *lst, char **split)
-//{
-//	static unsigned i;
-//	mu_assert_string_eq(split[i], value);
-//	i++;
-//
-//}
+MU_TEST(test_ft_strreplace)
+{
+	char	*str;
 
-//MU_TEST(test_ft_tokenizer)
-//{
-//	t_list		*tmp;
-//	t_list		*token_lst;
-//	char		*token_split[] = { "echo", "here we go" };
-//	unsigned	i;
-//	char		*value;
-//	char str[] = "echo 'here we go'";
-//
-//	token_lst = tokenizer(str);
-//	tmp = token_lst;
-//	i = 0;
-//	while (token_split[i])
-//	{
-//		if (token_lst->content == NULL)
-//			mu_fail("Incorrect parsing length.");
-//		value = ((t_token *)(token_lst->content))->value;
-//		if (value == NULL)
-//			mu_fail("Incorrect parsing length.");
-//		mu_assert_string_eq(token_split[i], value);
-//		token_lst = token_lst->next;
-//		i++;
-//	}
-//	if (token_lst->content)
-//		mu_fail("Incorrect parsing length.");
-//	ft_lstclear(&tmp, free);
-//}
+	str = ft_strdup("replace this with something");
+	ft_strreplace(&str, "this", "that");
+	mu_assert_string_eq("replace that with something", str);
+	free(str);
 
-MU_TEST(test_ft_tokenizer)
+	str = ft_strdup("replace this with this something");
+	ft_strreplace(&str, "this", "that");
+	mu_assert_string_eq("replace that with that something", str);
+	free(str);
+
+	str = ft_strdup("replace lilstring");
+	ft_strreplace(&str, "lilstring", "bigbigbigstring");
+	mu_assert_string_eq("replace bigbigbigstring", str);
+	free(str);
+}
+
+MU_TEST(test_tokenizer)
 {
 	t_list		*tokens;
 	t_list		*head;
-	char		str[] = "echo>>    here    we    go; 'single quotes <<<";
+	char		str[] = " echo>>    here    we    go; 'single quotes <<<";
 
 	tokens = tokenizer(str);
 	head = tokens;
-	mu_assert_string_eq("echo", tokens->content);
+	mu_assert_string_eq("echo", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq(">>", tokens->content);
+	mu_assert_string_eq(">>", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq("here", tokens->content);
+	mu_assert_string_eq("here", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq("we", tokens->content);
+	mu_assert_string_eq("we", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq("go", tokens->content);
+	mu_assert_string_eq("go", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq(";", tokens->content);
+	mu_assert_string_eq(";", ((t_token *)tokens->content)->value);
 	tokens = tokens->next;
-	mu_assert_string_eq("'single quotes <<<", tokens->content);
-	ft_lstclear(&head, free);
+	mu_assert_string_eq("'single quotes <<<", ((t_token *)tokens->content)->value);
+	ft_lstclear(&head, (void *)free_token);
+}
+
+MU_TEST(test_lexical_analysis)
+{
+	t_token	tk;
+
+	tk.value = ">>";
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_APPEND_OUTPUT, tk.type);
+
+	tk.value = ">";
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_REDIRECT_OUTPUT, tk.type);
+
+	tk.value = "<";
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_REDIRECT_INPUT, tk.type);
+
+	tk.value = ft_strdup("$CAKE");
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_LITERAL, tk.type);
+	mu_assert_string_eq("strawberry", tk.value);
+	free(tk.value);
+
+	tk.value = ft_strdup("\"$USER, this cake has $CAKE flavor.\"");
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_LITERAL, tk.type);
+	mu_assert_string_eq("gariadno, this cake has strawberry flavor.", tk.value);
+	free(tk.value);
+
+	tk.value = ft_strdup("\"This cake has $NOTHING flavor.\"");
+	lexical_analysis(&tk, htenv);
+	mu_assert_int_eq(T_LITERAL, tk.type);
+	mu_assert_string_eq("This cake has  flavor.", tk.value);
+	free(tk.value);
 }
 
 MU_TEST_SUITE(test_suite_tokens)
 {
+	MU_SUITE_CONFIGURE(&setup, &teardown);
 	MU_RUN_TEST(test_ft_strspn);
 	MU_RUN_TEST(test_ft_strcspn);
 	MU_RUN_TEST(test_ft_strtok);
-	MU_RUN_TEST(test_ft_tokenizer);
+	MU_RUN_TEST(test_ft_strreplace);
+	MU_RUN_TEST(test_tokenizer);
+	MU_RUN_TEST(test_lexical_analysis);
 }
-
 
 int	main(void)
 {
