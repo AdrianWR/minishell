17a65261251dde6d19b901c068e6a57c65004c6b
Adrian W. Roque <adrian.w.roque@gmail.com>
2021-03-03 23:32:35 -0300

    Refactor echo function

--- src/echo.c
+++ src/echo.c
@@ -6,28 +6,13 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 22:36:28 by aroque            #+#    #+#             */
-/*   Updated: 2021/02/07 23:18:32 by aroque           ###   ########.fr       */
+/*   Updated: 2021/03/03 23:27:48 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdbool.h>
 #include "libft.h"
 #include "minishell.h"
-
-//void	ft_echo(t_shell *shell)
-//{
-//	int	flag;
-//
-//	flag = 0;
-//	if (ft_streq(*shell->args, "-n"))
-//		flag = 1;
-//	if (!flag)
-//		shell->args++;
-//	while (*shell->args)
-//		ft_putstr_fd(*shell->args++, shell->fd);
-//	if (flag)
-//		ft_putchar_fd('\n', shell->fd);
-//}
 
 #define NO_NL_OPTION	"-n "
 #define EMPTY			""
