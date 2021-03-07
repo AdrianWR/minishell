42d18669ad64f3f0785dd5c13f3e1fa5d992b9ca
Adrian W. Roque <adrian.w.roque@gmail.com>
2021-03-07 01:39:30 -0300

    Refactor job data structure

--- include/job.h
+++ include/job.h
@@ -6,22 +6,23 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:46:01 by aroque            #+#    #+#             */
-/*   Updated: 2021/03/06 17:51:02 by aroque           ###   ########.fr       */
+/*   Updated: 2021/03/07 01:30:45 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef JOB_H
 # define JOB_H
 
-# include	"libft.h"
-# include	"process.h"
+# include "libft.h"
+# include "process.h"
+# include "token.h"
 
 typedef struct			s_job
 {
 	t_process			*process_list;
-	struct s_job 		*next;
+	struct s_job		*next;
 }						t_job;
 
-t_job					*parser(t_list *tokens);
+t_job					*parser(t_token *tokens);
 
 #endif
