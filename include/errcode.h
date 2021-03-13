e1a40960e1a7a023659f0db5d63d61d8fac18c59
Adrian W. Roque <adrian.w.roque@gmail.com>
2021-03-13 18:40:50 -0300

    Add invalid file exception

--- include/errcode.h
+++ include/errcode.h
@@ -6,7 +6,7 @@
 /*   By: aroque <aroque@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 23:36:03 by aroque            #+#    #+#             */
-/*   Updated: 2021/02/28 17:49:29 by aroque           ###   ########.fr       */
+/*   Updated: 2021/03/10 23:17:28 by aroque           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ typedef enum		e_errcode {
 	ERRSYS,
 	EUSAGE,
 	ECMDNF,
+	ENOFDI,
 	EPARSE,
 	EUNFQT,
 	__ERRMAX
@@ -28,6 +29,7 @@ static char *const	g_errstr[__ERRMAX] = {
 	"__ERRSYS",
 	"Usage: ./minishell",
 	"Command not found...",
+	"No such file or directory: ",
 	"Parse error",
 	"Unfinished quote"
 };
